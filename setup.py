e96a00ab11522e09ca1ece6a810f2b5fa7d0b4ca
Epsp <idontcare00110011@gmail.com>
2020-04-26 13:38:39 -0400

    new file:   auto-tag-anime.py 	modified:   setup.py 	deleted:    autoTagAnime.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 setup(
    name='auto-tag-anime',
    version='1.0',
+   author='epsp',
+   url='https://github.com/Epsp0/auto-tag-anime',
    description='adds tags to anime images predicted by DeepDanbooru tensorflow model',
-   install_requires=['tensorflow == 2.0', 'numpy', 'pillow', ] + ["iptcinfo"] if "win" in sys.platform else ['xattr'],
+   install_requires=['tensorflow', 'numpy', 'pillow', 'iptcinfo; platform_system == "Windows"', 
+   'xattr; platform_system != "Windows"'],
 ) 
